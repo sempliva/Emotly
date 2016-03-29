ed85352755a4d9a9d4b3e00c760f6215e23fd8ee
Tiziana Sellitto <tiziana.sel@gmail.com>
2016-03-29 09:29:33 -0700

    Added User model and MongoDB Settings for DB name.

--- emotly.py
+++ emotly.py
@@ -3,10 +3,14 @@ Emotly
 
 DEED
 """
+import os
 from flask import Flask, request
 from werkzeug.contrib.fixers import ProxyFix
+from flask.ext.mongoengine import MongoEngine
 
 app = Flask(__name__)
+app.config["MONGODB_SETTINGS"] = {'DB': os.environ['EMOTLY_DB_NAME']}
+db = MongoEngine(app)
 
 @app.route("/")
 def index():
