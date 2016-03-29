a1219b10bf36d0d9e77f947a0ba5e764617623ae
Giovanni Giuseppe Costa <giovannigcosta@gmail.com>
2016-03-29 09:29:33 -0700

    The project has been 'modularized'. Yeee!:)

---
+++ Emotly/models.py
@@ -0,0 +1,11 @@
+from Emotly import db
+
+
+class User(db.Document):
+    nickname = db.StringField(regex='^[a-z0-9A-Z]+$',
+                              min_length=6, max_length=15,
+                              required=True, unique=True
+                              )
+    email = db.EmailField(required=True, unique=True)
+    password = db.StringField(min_length=8, required=True)
+    salt = db.StringField(required=True)
