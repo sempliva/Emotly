de10568ade6cbeecef9869bd0d6d8f7d7b0d6d6e
Giovanni <ggc@MacBook-Pro-di-Giovanni.local>
2016-03-30 21:19:05 +0100

    moved models and functions from userController.py and models.py

--- emotly.py
+++ emotly.py
@@ -4,16 +4,28 @@ Emotly
 DEED
 """
 import os
-from flask import Flask, request
+from flask import Flask, request, abort
 from werkzeug.contrib.fixers import ProxyFix
 from flask.ext.mongoengine import MongoEngine
-from userController import user_controller
+import bcrypt
+
 
 app = Flask(__name__)
 app.config["MONGODB_SETTINGS"] = {'DB': os.environ['EMOTLY_DB_NAME']}
 db = MongoEngine(app)
 
-app.register_blueprint(user_controller)
+########MODELS##########
+
+
+class User(db.Document):
+    nickname = db.StringField(min_length=6)
+    email = db.EmailField(required = True, unique = True)
+    password = db.StringField(required = True)
+    salt = db.StringField(required = True)
+
+
+#######ROUTE###########
+
 
 @app.route("/")
 def index():
@@ -23,6 +35,29 @@ def index():
 def echo(echostr):
     return "Hello %s" % echostr
 
+@app.route('/singup', methods=['POST'])
+def signUp():
+
+    try:
+        nickname = request.form['username']
+        pwd = request.form['pwd'].encode('utf-8')
+        email = request.form['email']
+    except:
+        abort(401)
+
+    if len(User.objects.filter(email=email))>0:
+        return 'user already exist'
+
+    salt = bcrypt.gensalt()
+    hash = bcrypt.hashpw(pwd, salt)
+    user = User(
+        nickname=nickname,
+        password=hash,
+        salt=salt,
+        email=email
+    )
+    user.save()
+    return 'OK'
 # Gunicorn
 app.wsgi_app = ProxyFix(app.wsgi_app)
 
