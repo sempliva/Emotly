c0ac69c11d60d2c15076627fb31fd399b03fc345
Giovanni <ggc@MacBook-Pro-di-Giovanni.local>
2016-03-31 23:38:22 +0100

    user model moved in emotly.py

--- models.py
+++
@@ -1,7 +0,0 @@
-from emotly import db
-
-class User(db.Document):
-    nickname = db.StringField(min_length=6, unique = True)
-    email = db.EmailField(required = True, unique = True)
-    password = db.StringField(required = True)
-    salt = db.StringField(required = True)
