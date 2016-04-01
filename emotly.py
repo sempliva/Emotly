78739b1f0ac6ec7e6292d1ce97667b065801f86a
ggc87 <giovannigcosta@gmail.com>
2016-04-01 10:56:48 +0100

    code style fix

--- emotly.py
+++ emotly.py
@@ -5,37 +5,37 @@ DEED
 """
 import os
 import bcrypt
-from flask import Flask, request, render_template, \
-    abort, flash, redirect, url_for
+from flask import Flask, request, render_template, flash
 from werkzeug.contrib.fixers import ProxyFix
 from flask.ext.mongoengine import MongoEngine
 
-app=Flask(__name__)
-app.config["MONGODB_SETTINGS"]={
+app = Flask(__name__)
+app.config["MONGODB_SETTINGS"] = {
     'host': os.environ['EMOTLY_DB_URI'],
-    'username' : os.environ['EMOTLY_DB_USERNAME'],
-    'password' : os.environ['EMOTLY_DB_PASSWORD']
+    'username': os.environ['EMOTLY_DB_USERNAME'],
+    'password': os.environ['EMOTLY_DB_PASSWORD']
 }
-app.secret_key='super secret key'
-app.config['SESSION_TYPE']='filesystem'
+app.secret_key = 'super secret key'
+app.config['SESSION_TYPE'] = 'filesystem'
 
-db=MongoEngine(app)
+db = MongoEngine(app)
 
-########MODELS##########
+# ******MODELS******
 
 
 class User(db.Document):
-    nickname=db.StringField(min_length=6, unique=True)
-    email=db.EmailField(required=True, unique=True)
-    password=db.StringField(min_length=8, required=True)
-    salt=db.StringField(required=True)
+    nickname = db.StringField(min_length=6, unique=True)
+    email = db.EmailField(required=True, unique=True)
+    password = db.StringField(min_length=8, required=True)
+    salt = db.StringField(required=True)
 
-#######ROUTE###########
+# ******ROUTE******
 
 
 @app.route("/")
 def index():
     return render_template("page-home.html")
+
 
 @app.route("/signup", methods=["GET", "POST"])
 def signup():
@@ -50,21 +50,23 @@ def signup():
         flash('Registration error: %s' % e)
     return render_template("page-home.html")
 
-def register_user(req):
-    req_nickname=req.form['inputNickname']
-    req_pwd=req.form['inputPassword'].encode('utf-8')
-    req_email=req.form['inputEmail']
 
-    # TODO: This defaults to 12 rounds; should we have a configuration parameter?
-    salt=bcrypt.gensalt()
-    hash=bcrypt.hashpw(req_pwd, salt)
-    user=User(nickname=req_nickname, password=hash, salt=salt,
-            email=req_email)
+def register_user(req):
+    req_nickname = req.form['inputNickname']
+    req_pwd = req.form['inputPassword'].encode('utf-8')
+    req_email = req.form['inputEmail']
+
+    # TODO: This defaults to 12 rounds; should we have a configuration
+    # parameter?
+    salt = bcrypt.gensalt()
+    hash_pwd = bcrypt.hashpw(req_pwd, salt)
+    user = User(nickname=req_nickname, password=hash_pwd, salt=salt,
+                email=req_email)
 
     user.save()
 
 # Gunicorn
-app.wsgi_app=ProxyFix(app.wsgi_app)
+app.wsgi_app = ProxyFix(app.wsgi_app)
 
 if __name__ == "__main__":
     app.run(debug=True)
