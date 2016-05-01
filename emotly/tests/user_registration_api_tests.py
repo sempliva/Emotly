afb68ce45693476255a86cb3d2a29cb6eee2dcac
Tiziana Sellitto <tiziana.sel@gmail.com>
2016-05-01 16:25:47 -0700

    Added some tests for user registration controller and API.

--- emotly/tests/user_registration_api_tests.py
+++ emotly/tests/user_registration_api_tests.py
@@ -30,6 +30,17 @@ class UserRegistrationAPITestCase(unittest.TestCase):
                            headers=headers)
         self.assertEqual(rv.status_code, 200)
 
+    def test_signup_email_is_lower(self):
+        headers = {'content-type': 'application/json'}
+        data = json.dumps(dict(inputNickname='nicknametest12',
+                               inputEmail='Email1@EMAILTEST.com',
+                               inputPassword='password'))
+        rv = self.app.post(CONSTANTS.REST_API_PREFIX + '/signup',
+                           data=data,
+                           headers=headers)
+        user = User.objects.get(nickname="nicknametest12")
+        self.assertEqual(user.email, "Email1@EMAILTEST.com".lower())
+
     def test_signup_incomplete_request(self):
         headers = {'content-type': 'application/json'}
         data = json.dumps(dict(inputNickname="incompleterequest",
@@ -43,6 +54,26 @@ class UserRegistrationAPITestCase(unittest.TestCase):
         headers = {'content-type': 'application/json'}
         data = json.dumps(dict(inputNickname="short",
                                inputEmail="email@short.com",
+                               inputPassword="password"))
+        rv = self.app.post(CONSTANTS.REST_API_PREFIX + '/signup',
+                           data=data,
+                           headers=headers)
+        self.assertEqual(rv.status_code, 400)
+
+    def test_cannot_signup_space_in_nickname(self):
+        headers = {'content-type': 'application/json'}
+        data = json.dumps(dict(inputNickname="space in nickname ",
+                               inputEmail="email@short.com",
+                               inputPassword="password"))
+        rv = self.app.post(CONSTANTS.REST_API_PREFIX + '/signup',
+                           data=data,
+                           headers=headers)
+        self.assertEqual(rv.status_code, 400)
+
+    def test_cannot_signup_space_in_email(self):
+        headers = {'content-type': 'application/json'}
+        data = json.dumps(dict(inputNickname="spaceinemail",
+                               inputEmail="email@short.com ",
                                inputPassword="password"))
         rv = self.app.post(CONSTANTS.REST_API_PREFIX + '/signup',
                            data=data,
