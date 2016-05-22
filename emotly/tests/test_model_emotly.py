def28e17a68c3958b67e079e934df4de2ab37617
ggc87 <giovannigcosta@gmail.com>
2016-05-22 16:37:37 +0100

    Added GeoJson data to the emotly object
    Geodata now include accuracy and an optional location name

--- emotly/tests/test_model_emotly.py
+++ emotly/tests/test_model_emotly.py
@@ -29,50 +29,82 @@ import datetime
 import time
 from mongoengine import ValidationError
 from emotly import app
-from emotly.models import User, Emotly, MOOD
+from emotly.models import User, Emotly, Location, MOOD
 
 
 # Model: Emotly.
 class EmotlyModelTestCase(unittest.TestCase):
+    u = None
+
     def setUp(self):
         self.app = app.test_client()
+        self.u = User(nickname='testemotly',
+                      email='test_emotly@example.com',
+                      password="FakeUserPassword123",
+                      confirmed_email=True,
+                      last_login=datetime.datetime.now(),
+                      salt="salt")
+        self.u.save()
 
     def tearDown(self):
         User.objects.delete()
+        Emotly.objects.delete()
 
     def test_create_emotly(self):
-        u = User(nickname='testemotly',
-                 email='test_emotly@example.com',
-                 password="FakeUserPassword123",
-                 confirmed_email=True,
-                 last_login=datetime.datetime.now(),
-                 salt="salt")
-        u.save()
         emotly = Emotly(mood=1)
-        emotly.user = u
+        emotly.user = self.u
         self.assertTrue(emotly.save())
 
-    def test_create_3_emotly(self):
-        u = User(nickname='testemotly',
-                 email='test_emotly@example.com',
-                 password="FakeUserPassword123",
-                 confirmed_email=True,
-                 last_login=datetime.datetime.now,
-                 salt="salt")
-        u.save()
+    def test_create_emotly_with_valid_geodata(self):
+        emotly = Emotly(mood=1,
+                        geodata=Location(
+                            coord=[1, 1],
+                            accuracy=0.14,
+                            location_name="Don Ciro Pizza"))
+        emotly.user = self.u
+        self.assertTrue(emotly.save())
 
+    def test_create_emotly_with_invalid_location(self):
+        emotly = Emotly(mood=1,
+                        geodata=Location(
+                            coord=["abc", 1],
+                            accuracy=0.14))
+        emotly.user = self.u
+        with self.assertRaises(ValidationError):
+            emotly.save()
+
+    def test_create_emotly_with_invalid_accuracy(self):
+        emotly = Emotly(mood=1,
+                        geodata=Location(
+                            coord=[1, 1],
+                            accuracy="@"))
+        emotly.user = self.u
+        with self.assertRaises(ValidationError):
+            emotly.save()
+
+    def test_create_emotly_with_invalid_location_name(self):
+        emotly = Emotly(mood=1,
+                        geodata=Location(
+                            coord=[1, 1],
+                            accuracy=3.4,
+                            location_name=1))
+        emotly.user = self.u
+        with self.assertRaises(ValidationError):
+            emotly.save()
+
+    def test_create_3_emotly(self):
         emotly = Emotly(mood=1)
-        emotly.user = u
+        emotly.user = self.u
         emotly.save()
         time.sleep(1)  # sleep time in seconds
 
         emotly1 = Emotly(mood=2)
-        emotly1.user = u
+        emotly1.user = self.u
         emotly1.save()
         time.sleep(1)  # sleep time in seconds
 
         emotly2 = Emotly(mood=3)
-        emotly2.user = u
+        emotly2.user = self.u
         emotly2.save()
         self.assertNotEqual(emotly.created_at, emotly1.created_at)
         self.assertNotEqual(emotly1.created_at, emotly2.created_at)
@@ -83,27 +115,13 @@ class EmotlyModelTestCase(unittest.TestCase):
             emotly.save()
 
     def test_cannot_create_emotly_whitout_mood(self):
-        u = User(nickname='testmood',
-                 email='test_mood@example.com',
-                 password="FakeUserPassword123",
-                 confirmed_email=True,
-                 last_login=datetime.datetime.now(),
-                 salt="salt")
-        u.save()
         emotly = Emotly()
-        emotly.user = u
+        emotly.user = self.u
         with self.assertRaises(ValidationError):
             emotly.save()
 
     def test_cannot_create_emotly_mood_not_valid(self):
-        u = User(nickname='testinvalid',
-                 email='test_mood_invalid@example.com',
-                 password="FakeUserPassword123",
-                 confirmed_email=True,
-                 last_login=datetime.datetime.now(),
-                 salt="salt")
-        u.save()
         emotly = Emotly(mood=len(MOOD) + 1)
-        emotly.user = u
+        emotly.user = self.u
         with self.assertRaises(ValidationError):
             emotly.save()
